f0ad7b6e539e22bf4e7e2b5d697fe592518de7aa
Seung Hwan Oh <steve2@Seungs-MacBook-Air.local>
2024-08-04 06:12:46 +0900

    first commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ load_dotenv()
 NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
 NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 
 from search import get_naver_search_results
@@ -21,6 +21,19 @@ def search():
     query = request.form['query']
     results = get_naver_search_results(query)
     return render_template('results.html', query=query, results=results)
+
+
+@app.route('/search_api', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    results, code = get_naver_search_results(query)
+
+    if code == 200:
+        return results
+    else:
+        return jsonify({"error": "Error Code:" + str(code)})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- search.py
+++ search.py
@@ -9,8 +9,7 @@ NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
 
 
 def get_naver_search_results(query):
-    # client_id = "YOUR_CLIENT_ID"
-    # client_secret = "YOUR_CLIENT_SECRET"
+
     client_id = NAVER_CLIENT_ID
     client_secret =NAVER_CLIENT_SECRET
 
@@ -20,11 +19,15 @@ def get_naver_search_results(query):
         "X-Naver-Client-Secret": client_secret
     }
     params = {"query": query}
+
     response = requests.get(url, headers=headers, params=params)
-    return response.json()
+    return response.json(), response.status_code
+  
+    
 
 query = "성신여대"
-results = get_naver_search_results(query)
+results, code = get_naver_search_results(query)
+print(code)
 print(len(results['items']))
 print(results['items'][0]['title'])
 
